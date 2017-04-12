ddbdb600f50780ef1affab6e0d0467917ccd2496
Sean hoyt <seanhoyt963@gmail.com>
2017-04-12 02:05:02 +0000

    Fix the screen rotation in the kernel
    Change-Id: I806bec479adbce5be89707ed27a2fccd8766013f

--- system_prop.mk
+++ system_prop.mk
@@ -30,8 +30,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=160 \
     ro.opengles.version=196608 \
     ro.qualcomm.cabl=0 \
-    ro.hwui.text_large_cache_height=2048 \
-    ro.sf.hwrotation=270
+    ro.hwui.text_large_cache_height=2048
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
